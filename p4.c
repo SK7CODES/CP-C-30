1a3b43121e4c5abe7330a8256ef334775e306d3f
SK7CODES <103025982+SK7CODES@users.noreply.github.com>
2022-04-12 13:09:21 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,35 @@
+// This program is the application of if else statement  //
+
+
+#include <stdio.h>
+int main ()
+{
+    int a,b,sum ;
+    printf ("Enter the value of a and b \n") ;
+    scanf ("%d%d" ,&a ,&b );
+    sum = a + b ;
+    printf ("Sum of a and b is %d\n", sum);
+    if (sum>20)
+    {
+        printf("Sum is greater than 20 \n"); 
+    }
+      else if (sum > 10)
+      {
+          printf("Sum is greater than 10 and less than 20\n" );
+      }
+      else
+      {
+          printf("Sum is less than 10\n");
+        }
+        
+ return 0 ;
+}
+
+
+//Output//
+
+Enter the value of a and b
+4
+5
+Sum of a and b is 9
+Sum is less than 10
