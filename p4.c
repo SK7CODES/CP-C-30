69375632f7444a0fec32d412c716a271e0c1cb3b
SK7CODES <103025982+SK7CODES@users.noreply.github.com>
2022-04-12 12:22:01 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,16 @@
+// This program is to find sum of two numbers //
+#include <stdio.h>
+int add(int a, int b )
+{
+int c = a + b ;
+}
+int main()
+{
+int sum ;
+sum=add(7,7) ;
+printf("Sum is %d\n",sum);
+return 0 ;
+}
+
+//Output//
+Sum is 14
