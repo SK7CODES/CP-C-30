2bdd72ca2c27ef678949020126bbfd3a173b3ee4
SK7CODES <103025982+SK7CODES@users.noreply.github.com>
2022-04-17 19:55:19 +0530

    Add files via upload

---
+++ P4-(Scope of Variables).c
@@ -0,0 +1,17 @@
+// This program is to find sum of two numbers //
+
+#include <stdio.h>
+int a=7;
+int b=4;
+void add()
+{
+    int sum;
+    sum = a + b;
+    printf("Sum of a and b is %d\n",sum);
+    
+}
+int main()
+{
+add();
+return 0;
+}
