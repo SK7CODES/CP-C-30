192680f1cc616cdd45f048ba195cfaa3366b5059
SK7CODES <103025982+SK7CODES@users.noreply.github.com>
2022-05-09 21:43:02 +0530

    Update P9-(Relational Operators).c

--- P9-(Relational Operators).c
+++ P9-(Relational Operators).c
@@ -15,8 +15,6 @@ else if (a>=b)
 printf("%d is greater than or equal to %d\n",a,b);
 if (a==b)
 printf("%d is equal to %d \n", a,b);
-else if(a !=b)
-printf("%d is not equal to %d \n",a,b);
 else if(a!=b)
 printf("%d is not equal to %d\n",a,b);
 return 0;
