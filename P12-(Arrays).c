d1478e914e79b374adec9391e974fbe0f3a48af6
SK7CODES <103025982+SK7CODES@users.noreply.github.com>
2022-05-10 12:35:35 +0530

    Add files via upload

---
+++ P12-(Arrays).c
@@ -0,0 +1,15 @@
+// This program is to show the appllication of  Arrays //
+
+
+#include<stdio.h>
+int main()
+{
+    int star[3]={4,5,6};
+    int sum;
+    sum=star[0]+star[1]+star[2];
+    printf("The sum is %d\n",sum);
+    return 0;
+}
+
+Output-
+The sum is 15
