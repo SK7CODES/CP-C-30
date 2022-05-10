0deb4f1b57df26b631976cce7a1fc1915cfe39ab
SK7CODES <103025982+SK7CODES@users.noreply.github.com>
2022-05-10 11:46:39 +0530

    Add files via upload

---
+++ P11-1-(Loops).c
@@ -0,0 +1,29 @@
+// This program is to show the appllication of While Loop //
+
+#include<stdio.h>
+int main()
+{
+    int x=0;
+    int y=0;
+    while (x<=10)
+    {
+        y=y+x;
+        printf("%d\n",y);
+        x++;
+    }
+}
+
+
+
+Output-
+0
+1
+3
+6
+10
+15
+21
+28
+36
+45
+55
