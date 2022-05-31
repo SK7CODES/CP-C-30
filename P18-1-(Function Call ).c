497b19027781a3341a88e6565e4b90e59e40d0c1
SK7CODES <103025982+SK7CODES@users.noreply.github.com>
2022-05-31 12:56:55 +0530

    Add files via upload

---
+++ P18-1-(Function Call ).c
@@ -0,0 +1,30 @@
+/*This program is to show Function call by value in C*/
+
+
+#include<stdio.h>
+
+int cube(int z)
+
+{
+    
+ z=z*z*z;
+     
+ return(z);
+    
+
+}
+
+int main()
+
+{
+
+  int n=99;
+
+  printf("Cube of %d is %d\n",n,cube(n));
+
+    return 0;
+
+}
+
+Output-
+Cube of 99 is 970299
